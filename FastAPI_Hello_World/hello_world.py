eec156c6598034ae141c9486abd666d0fab836e4
Obie Ananda <Obie.kal22@gmail.com>
2025-03-17 22:48:11 +0700

    feat: implement uvicorn to run when executing the python scripts itself

--- FastAPI_Hello_World/hello_world.py
+++ FastAPI_Hello_World/hello_world.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 # declare an application object of FastAPI class
+# invoke with the command: uvicorn "FastAPI_Hello_World.hello_world:app" --reload
 app = FastAPI()
 
 @app.get("/")
